b2135b0a817b4d2070d8cf08e20f9d487e7b381e
John Tan <johntan@thoughtworks.com>
2019-08-18 22:59:08 +1000

    Manually create validatorsFromKnowledgeStatements.

---
+++ validatorsFromKnowledgeStatements.js
@@ -0,0 +1,20 @@
+const {
+  isNotTheBest,
+  isNotTheWorst,
+  isNotTheBestOrTheWorst,
+  isBetterThan,
+  isNotDirectlyBelowOrAbove,
+} = require('./validators');
+
+const validatorsFromKnowledgeStatements = [
+  isNotTheBest('Jessie'),
+  isNotTheWorst('Evan'),
+  isNotTheBestOrTheWorst('John'),
+  isBetterThan('Sarah', 'Evan'),
+  isNotDirectlyBelowOrAbove('Matt', 'John'),
+  isNotDirectlyBelowOrAbove('John', 'Evan'),
+];
+
+module.exports = {
+  validatorsFromKnowledgeStatements,
+};
